e03fc914e8dba9593c3ea64115fc46de6b14e440
CSCI 311 Team C <csci311c@stumail.csci.viu.ca>
2023-02-27 13:08:48 -0800

    Test sql tables

---
+++ mysql/tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(16) NOT NULL,
+	icon VARCHAR(512),
+	password VARCHAR(32),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE scores(
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT,
+	score INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
