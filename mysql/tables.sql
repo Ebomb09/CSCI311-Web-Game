4d26e040012872ff7eddbfdc25cf564e5b6e327b
CSCI 311 Team C <csci311c@pup14.csci.viu.ca>
2023-03-07 14:17:47 -0800

    More test data

--- mysql/tables.sql
+++ mysql/tables.sql
@@ -19,12 +19,33 @@ CREATE TABLE scores(
 );
 
 INSERT INTO users 
-	(name, password) 
+	(name, password, icon) 
 VALUES 
-	("jeff", "test123"),
-	("matt", "asdf"),
-	("jimjon", "iou"),
-	("bimathon", "lolol"),
-	("clark", "kent"),
-	("williamsonmanson", "password32")
+	("jeff", "test123", "icons/face1.png"),
+	("matt", "asdf", "icons/face2.png"),
+	("jimjon", "iou", "icons/face3.png"),
+	("bimathon", "lolol", "icons/face4.png"),
+	("clark", "kent", "icons/face5.png"),
+	("williamsonmanson", "password32", "icons/face6.png"),
+	("tahvo_teboho", "thisissecure_not", "icons/face2.png"),
+	("ma'toes", "2389u4891", "icons/face6.png"),
+	("sabinea", "1290854389", "icons/face3.png"),
+	("oissine", "aiurhgsghs832", "icons/face1.png"),
+	("daquan", "29u8wrg89", "icons/face5.png")
+;
+
+INSERT INTO scores
+	(user_id, score)
+VALUES
+	(1, 200),
+	(2, 1000),
+	(3, 500),
+	(4, 10),
+	(5, 1005),
+	(6, 69),
+	(7, 15),
+	(8, 908),
+	(9, 1001),
+	(10, 535),
+	(11, 1)
 ;
