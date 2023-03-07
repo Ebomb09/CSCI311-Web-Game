ef7ef43da77357cb5b8c8a3aeb889d16ff3de116
CSCI 311 Team C <csci311c@cub14.csci.viu.ca>
2023-03-06 18:09:36 -0800

    Install comments and TODO

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 #Install web interface stuff
+echo "Copying web project files"
+
 for file in ~/public_html/*; do
 	rm -rf $file
 done
+
 cp -rp www/* ~/public_html
+
+#Install MySQL tables
+#TODO Script to load login and load tables, use non-git tracked for security
+#TODO Options to load test data
+echo "Loading MySQL tables"
+
+#Done
+echo 'Done installing'
