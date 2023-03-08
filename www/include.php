772dc0009db457d6189442933e91e1941e5398ed
CSCI 311 Team C <csci311c@pup14.csci.viu.ca>
2023-03-07 20:21:33 -0800

    Ethan: Added db methods for getting users, scores and updating scores

--- www/include.php
+++ www/include.php
@@ -1,15 +1,16 @@
 <?php
-	$db_conf_dir = "../conf/db.info";
-	$db_conf = fopen($db_conf_dir, "r");
+	$db_conf_dir = trim(shell_exec('eval echo ~csci311c')) . '/project.git/conf/db.info';
+	$db_conf = fopen($db_conf_dir, 'r');
 	$db_host = trim(fgets($db_conf));
 	$db_user = trim(fgets($db_conf));
 	$db_pass = trim(fgets($db_conf));
 	$db_name = trim(fgets($db_conf));
 	fclose($db_conf);
 
-	function db_connect(){
+	function db_connect($db_host, $db_name, $db_user, $db_pass){
 
 		try{
+
 			$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 			return $db;
 		}
@@ -18,6 +19,72 @@
 		}
 
 		return false;
+	}
+
+	function db_getUsers($db, $id = false, $name = false){
+
+		$results = false;
+
+		if (!$db)
+			return $results;
+
+		if ($userid)
+			$results = $db->query("SELECT * FROM users WHERE id=$id;");
+		
+		elseif ($name)
+			$results = $db->query("SELECT * FROM users WHERE name='$name';");
+
+		else
+			$results = $db->query("SELECT * FROM users;");
+ 
+		return $results;	
+	}
+
+	function db_getUsersByName($db, $name){
+		return db_getUsers($db, false, $name);
+	}
+
+	function db_getUsersById($db, $id){
+		return db_getUsers($db, $id, false);
+	}
+
+	function db_getUserScores($db){
+
+		$results = false;
+
+		if(!$db)
+			return $results;
+
+		$results = $db->query("SELECT * FROM users JOIN scores ON users.id = scores.user_id;");
+
+		return $results;
+	}
+
+	function db_addUserScore($db, $user_id, $score){
+
+		if(!$db)
+			return false;
+
+		$found = false;
+
+		// Check all user scores to see if already added
+		foreach(db_getUserScores($db) as $row){
+
+			if($row['user_id'] == $user_id){
+				$found = true;
+
+				if($row['score'] < $score){
+					$db->query("UPDATE scores SET score=$score WHERE user_id=$user_id;");
+					break;
+				}
+			}
+		}
+
+		// Try to add if not found
+		if(!$found)
+			$db->query("INSERT INTO scores(user_id, score) VALUES ($user_id, $score);");
+
+		return true;
 	}
 ?>
 
--- www/scores.php
+++ www/scores.php
@@ -11,21 +11,39 @@
 
 			<tr>
 				<th> User </th>
+				<th> <!-- Icon --> </th>
 				<th> Score </th>
 			</tr>
 
 			<?php
-				require 'include.php';
+				require_once 'include.php';
 
-				$db = db_connect();
-				
-				foreach ($db->query("SELECT * FROM scores") as $row){
+				$db = db_connect($db_host, $db_name, $db_user, $db_pass);
 
-					$userID = $row['user_id'];
+				// Check if there are any scores to submit
+				if($_GET['submit'] == '1'){
+
+					// Generic type checking
+					if(is_numeric($_GET['score']) && is_numeric($_GET['user_id']) ){
+
+						// Cast to appropriate typings
+						$user = $_GET['user_id'];
+						$score = (int)$_GET['score'];
+
+						// Try to add user score
+						db_addUserScore($db, (int)$_GET['user_id'], (int)$_GET['score']);
+					}
+				}
+
+				foreach (db_getUserScores($db) as $row){
+
+					$userID = $row['name'];
 					$score = $row['score'];
+					$icon = $row['icon'];
 
 					print "<tr>";
 					print "<td> $userID </td>";
+					print "<td> <img src='images/$icon' alt='$userID profile icon'> </td>";
 					print "<td> $score </td>";
 					print "</tr>";
 				}
