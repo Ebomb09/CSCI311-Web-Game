32e3cdd25ffe48ecac44374d474829e669915157
CSCI 311 Team C <csci311c@cub14.csci.viu.ca>
2023-03-07 18:18:41 -0800

    Seperated installation dir to just www

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Set the project installation directory
-install_dir=~/public_html
-echo "Installing contents to $install_dir"
-
 # Install MySQL tables
 db_conf_file=conf/db.info
 
-echo "Reading MySQL configuration $conf_file"
+echo "Reading MySQL configuration $db_conf_file"
 db_host=$(sed -n 1p $db_conf_file)
 db_user=$(sed -n 2p $db_conf_file)
 db_pass=$(sed -n 3p $db_conf_file)
