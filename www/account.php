98393578fe79c355d099219d94019332ef7b7fd7
Ethan Bryson <brysone@stumail.csci.viu.ca>
2023-03-09 17:10:41 -0800

    Seperated handling on account login,creation, and modifying events

--- www/account.php
+++ www/account.php
@@ -11,51 +11,9 @@
 			require_once 'include/db.php';
 
 			$db = db_connect($db_host, $db_name, $db_user, $db_pass);
-			$user_id = getcookie('user_id');
-			$logged_in = false;		
+			$user_id = db_handleAccount($db);
 
-			$event_type = getPOST('type');
-			$event_user = getPOST('username');
-			$event_pass = getPOST('password');
-
-			switch($event_type){
-
-				case 'login': 
-
-					foreach(db_getUsersByName($db, $event_user) as $row){
-
-						if($row['password'] == $event_pass){
-							setcookie('user_id', $row['id']);
-							$user_id = $row['id'];
-						}
-					}
-					break;
-
-				case 'create':
-
-					if(db_addUser($db, $event_user, $event_pass)){
-
-						// Check if actual user account was created
-						foreach(db_getUsersByName($db, $event_user) as $row){
-							
-							if($row['password'] == $event_pass){
-								setcookie('user_id', $row['id']);
-								$user_id = $row['id'];
-							}
-						}
-					}
-					break;
-
-				case 'logout':
-					setcookie('user_id', '');
-					$user_id = 0;
-					break;
-			}
-
-			if($user_id > 0 && db_getUsersById($db, $user_id)->rowCount() > 0)
-				$logged_in = true;
-
-			if($logged_in)
+			if($user_id)
 				require 'account-manage.php';
 			else
 				require 'account-login.php';
--- www/include/db.php
+++ www/include/db.php
@@ -123,4 +123,54 @@
 
 		return true;
 	}
+
+	function db_handleAccount($db){
+
+		if(!$db)	
+			return false;
+
+		$user_id = getcookie('user_id');
+		$event_type = getPOST('type');
+		$event_user = getPOST('username');
+		$event_pass = getPOST('password');
+
+		switch($event_type){
+
+			case 'login': 
+
+				foreach(db_getUsersByName($db, $event_user) as $row){
+
+					if($row['password'] == $event_pass){
+						setcookie('user_id', $row['id']);
+						$user_id = $row['id'];
+					}
+				}
+				break;
+
+			case 'create':
+
+				if(db_addUser($db, $event_user, $event_pass)){
+
+					// Check if actual user account was created
+					foreach(db_getUsersByName($db, $event_user) as $row){
+						
+						if($row['password'] == $event_pass){
+							setcookie('user_id', $row['id']);
+							$user_id = $row['id'];
+						}
+					}
+				}
+				break;
+
+			case 'logout':
+				setcookie('user_id', '');
+				$user_id = 0;
+				break;
+		}
+
+		if($user_id > 0 && db_getUsersById($db, $user_id)->rowCount() == 0)
+			$user_id = 0;
+
+		return $user_id;
+	}
 ?>
