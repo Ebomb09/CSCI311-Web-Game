58cf8df31f68ccb4af6e7cd7ec39c23c46420911
RememberButterflies <121469888+RememberButterflies@users.noreply.github.com>
2023-03-10 19:13:07 -0800

    Update account-login.php
    whitespace

--- www/account-login.php
+++ www/account-login.php
@@ -5,12 +5,11 @@
 		<div class="usrtxt">
 			<label for="username">Username</label>
 			<input name="username" type="text" required><br>
-
 			<label for="password">Password</label>
 			<input name="password" type="password" required><br>
 		</div>		
 		<div class="button">
-		<input name="type" type="hidden" value="login">
+			<input name="type" type="hidden" value="login">
 			<input type="submit" value="Login">
 		</div>
 	</form>
@@ -23,7 +22,6 @@
 		<div class="usrtxt">
 			<label for="username">Username</label>
 			<input name="username" type="text" required><br>
-
 			<label for="password">Password</label>
 			<input name="password" type="password" required><br>
 		</div>		
