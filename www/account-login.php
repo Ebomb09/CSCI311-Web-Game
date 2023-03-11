16e217df7d3604794640ca2d624bdd0e586995d7
RememberButterflies <121469888+RememberButterflies@users.noreply.github.com>
2023-03-10 19:09:29 -0800

    Update account-login.php
    putting input fields into divs for centering

--- www/account-login.php
+++ www/account-login.php
@@ -2,32 +2,34 @@
 	<h1> Login </h1>
 
 	<form method="POST">
+		<div class="usrtxt">
+			<label for="username">Username</label>
+			<input name="username" type="text" required><br>
 
-		<label for="username">Username</label>
-		<input name="username" type="text" required>
-
-		<label for="password">Password</label>
-		<input name="password" type="password" required>
-
+			<label for="password">Password</label>
+			<input name="password" type="password" required><br>
+		</div>		
+		<div class="button">
 		<input name="type" type="hidden" value="login">
-
-		<input type="submit" value="Login">
+			<input type="submit" value="Login">
+		</div>
 	</form>
 </div>
 
 <div class="create_account">
-	<p> Don't have an account? </p>
-	<h2> Create an Account </h2>
+	<h1> Create an Account </h1>
 
 	<form method="POST">
-		<label for="username">Username</label>
-		<input name="username" type="text" required>
+		<div class="usrtxt">
+			<label for="username">Username</label>
+			<input name="username" type="text" required><br>
 
-		<label for="password">Password</label>
-		<input name="password" type="password" required>
-
-		<input name="type" type="hidden" value="create">
-
-		<input type="submit" value="Create Account">
+			<label for="password">Password</label>
+			<input name="password" type="password" required><br>
+		</div>		
+		<div class="button">
+			<input name="type" type="hidden" value="create">
+			<input type="submit" value="Create Account">
+		</div>
 	</form>
 </div>
