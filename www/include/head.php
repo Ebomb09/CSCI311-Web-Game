6ff61576a9c505dd75b127d522cbf6c1e2310187
Ethan Bryson <brysone@stumail.csci.viu.ca>
2023-03-28 14:03:11 -0700

    Added navigation pane

--- www/include/head.php
+++ www/include/head.php
@@ -1,3 +1,9 @@
+<?php
+	require_once 'db.php';
+	$db = db_connect($db_host, $db_name, $db_user, $db_pass);
+	$user_id = db_handleAccount($db);
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -6,6 +12,27 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title> <?php echo $page_name; ?> </title>
+	<link rel="stylesheet" href="css/teststyles.css">
 </head>
 
 <body>
+
+<nav>
+	<h1> CSCI311 Web Game </h1>
+	<ul>
+		<li> <a href='index'> Play Game </a> </li>
+		<li> <a href='scores'> View High Scores </a> </li>
+
+		<?php
+
+		if($user_id){
+			$result = db_getUsersById($db, $user_id);
+
+			if($result->rowCount() == 1){
+				$name = $result->fetch()['name'];
+				echo "<li> <a href='account'> $name </a> </li>";
+			}
+		}
+		?>
+	</ul>
+</nav>
