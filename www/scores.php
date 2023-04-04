3614d43ca94722f2443723959220d36bbfc5f631
RememberButterflies <121469888+RememberButterflies@users.noreply.github.com>
2023-04-03 21:06:14 -0700

    Update scores.php
    new mysql query that joins users and scores tables. scores are ordered descending. top 3 players names and scores are bolded.

--- www/scores.php
+++ www/scores.php
@@ -17,45 +17,70 @@
 		<th class="score"> Score </th>
 	</tr>
 
-	<?php
-		// Check if there are any scores to submit
-		$score_added = false;
+<?php
 
-		if($_SERVER['REQUEST_METHOD'] == 'POST'){
+// Check if there are any scores to submit
+$score_added = false;
 
-			// Generic type checking
-			if($user_id && is_numeric($_POST['score'])){
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-				// Cast to appropriate typings
-				$score = (int)$_POST['score'];
+	// Generic type checking
+	if($user_id && is_numeric($_POST['score'])){
 
-				// Try to add user score
-				db_addUserScore($db, $user_id, $score);
-				$score_added = true;
-			}
-		}
+		// Cast to appropriate typings
+		$score = (int)$_POST['score'];
 
-		// Get all scores from database and append to the table
-		foreach (db_getUserScores($db) as $row){
+		// Try to add user score
+		db_addUserScore($db, $user_id, $score);
+		$score_added = true;
+	}
+}
 
-			$scorer_id = $row['user_id'];
-			$name = $row['name'];
-			$score = $row['score'];
-			$icon = $row['icon'];
 
-			// For now how to show what score was added
-			if($score_added && $user_id == $scorer_id)
-				$name = '>>' . $name . '<<';
+// MySQL query
+$sql = "SELECT u.name, u.icon, s.score, s.user_id FROM users AS u JOIN scores AS s ON u.id = s.user_id ORDER BY s.score DESC, u.name ASC";
 
+// Prepare statement
+$stmt = $db->prepare($sql);
+
+// Execute statement
+$stmt->execute();
+
+$i = 1;
+// Check if any rows were returned
+if ($stmt->rowCount() > 0) {
+    // Output data of each row
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+        	$icon = $row["icon"];
+        	$score = $row["score"];        	
+        	$name = $row["name"];
+	    // Top 3 scores
+		if($i < 4){
+			print "<tr>";
+			print "<td class='icon'> <img src='images/$icon' alt='$name profile icon'> </td>";
+			print "<td class='username'><b> $name </b></td>";
+			print "<td class='score'><b> $score </b></td>";
+			print "</tr>";
+	// The rest
+		} else {
 			print "<tr>";
 			print "<td class='icon'> <img src='images/$icon' alt='$name profile icon'> </td>";
 			print "<td class='username'> $name </td>";
 			print "<td class='score'> $score </td>";
 			print "</tr>";
 		}
-	?>
+		$i++;
+    }
+} else {
+    echo "0 results";
+}
 
+// Close statement
+$stmt = null;
+?>
 </table>
+
 
 
 <?php
